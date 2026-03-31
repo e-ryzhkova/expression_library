eb70064f87a072107a50870a7d91a694d3d85c65
grechka <s.cheltushev@g.nsu.ru>
2026-03-31 16:04:33 +0700

    update 2

--- src/symbolic/simplify.c
+++ src/symbolic/simplify.c
@@ -15,8 +15,106 @@ ExprNode* simplify(ExprNode* node) {
             return node;
         
         case NODE_BINARY:
-            //
-            break;
+            node->data.binary.left = simplify(node->data.binary.left);
+            node->data.binary.right = simplify(node->data.binary.right);
+
+            ExprNode *right = node->data.binary.right;
+            ExprNode *left = node->data.binary.left;
+
+            char op = node->data.binary.op;
+
+            if (right->type == NODE_NUMBER && left->type == NODE_NUMBER){
+                double result;
+                double a = left->data.number; double b = right->data.number;
+
+                switch (op){
+
+                    case '+': result = a + b; break;
+
+                    case '-': result = a - b; break;
+
+                    case '*': result = a * b; break;
+
+                    case '/': result = a / b; break;
+
+                    case '^': result = pow(a, b); break;       
+                }
+
+                ExprNode * new_node = create_num_node(result);
+                free_node(node);
+
+                return new_node;
+            }
+
+            if (op == '+'){
+                if (left->type == NODE_NUMBER && left->data.number == 0){
+                    ExprNode *temp = node->data.binary.right;
+
+                    node->data.binary.left = NULL;
+                    node->data.binary.right = NULL;
+
+                    free_node(node);
+                    return temp;   
+
+                }   
+                if (right->type == NODE_NUMBER && right->data.number == 0){
+                    ExprNode *temp = node->data.binary.left;
+
+                    node->data.binary.right = NULL;
+                    node->data.binary.left = NULL;
+
+                    free_node(node);
+                    return temp;   
+
+                }
+            }
+
+            if (op == '-'){
+                if (left->type == NODE_NUMBER && left->data.number == 0){
+                    ExprNode *temp = create_un_node('-', node->data.binary.right);
+
+                    node->data.binary.right = NULL;
+                    node->data.binary.left = NULL;
+
+                    free_node(node);
+                    return temp;
+                }
+                
+                if (right->type == NODE_NUMBER && right->data.number == 0){
+                    ExprNode *temp = node->data.binary.left;
+
+                    node->data.binary.right = NULL;
+                    node->data.binary.left = NULL;
+
+                    free_node(node);
+                    return temp;
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         case NODE_FUNCTION:
             //
